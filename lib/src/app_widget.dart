70b171e2152205c2a155383352561a1ffe8b27f1
vitu <vitorcsharp@hotmail.com>
2023-11-26 14:58:23 -0300

    feat: adding our thema to the app_widget

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prova_flutter/src/core/theme/theme_config.dart';
 import 'package:prova_flutter/src/pages/home/home_page.dart';
+import 'package:prova_flutter/src/pages/information/information_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -9,9 +10,13 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Test',
-      theme:  ThemeConfig.theme,
-      home: const HomePage(),
+      theme: ThemeConfig.theme,     
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/informationPage': (context) => const InformationPage(),
+      }
     );
   }
 }
