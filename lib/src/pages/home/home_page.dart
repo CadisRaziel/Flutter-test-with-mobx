f197e2f0496eccad26de3e4aae017aff996f6849
vitu <vitorcsharp@hotmail.com>
2023-11-26 15:02:50 -0300

    feat: Creating our home page with what it should CONTAIN which is : 1) A password field - 2)A text field to represent the Login - 3)A Label described "privacy policy" and also how it should BEHAVE : 1)Check and warn that both the Login and Password fields are filled in (The password field cannot be less than two characters long - The password field cannot have any special characters, only 'a' to 'Z' and '0' to '9' can be entered) - 2) Both fields cannot exceed 20 characters - 3) Both fields cannot end with a space character at the end - 4) If both pieces of information are filled in, go to the next screen - 5) When you tap on the "Privacy policy" label, a web page directed to google.com.br should open

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:prova_flutter/src/core/helpers/size_extensions.dart';
+import 'package:prova_flutter/src/core/shared/url_launcher.dart';
 import 'package:prova_flutter/src/core/styles/colors_app.dart';
 import 'package:prova_flutter/src/core/styles/text_styles.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final TextEditingController _userController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _userController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onLoading() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              LoadingAnimationWidget.dotsTriangle(
+                color: context.colors.whiteAux,
+                size: 60,
+              ),
+              SizedBox(
+                height: context.percentHeight(.03),
+              ),
+              Text(
+                "Loading",
+                style: context.textStyles.textLight.copyWith(
+                  color: context.colors.whiteAux,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pop(context);
+    }).whenComplete(
+      () => Navigator.pushNamed(
+        context,
+        '/informationPage',
+      ),
+    );
+  }
+
+   var urlLauncher = UrlLauncher();
+  
 
   @override
   Widget build(BuildContext context) {
@@ -27,88 +88,127 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Spacer(),
-              Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        "Usuário",
-                        style: context.textStyles.textLight.copyWith(
-                          color: context.colors.textAux,
-                          fontSize: 16,
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: Text(
+                          "Usuário",
+                          style: context.textStyles.textLight.copyWith(
+                            color: context.colors.whiteAux,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.person,
-                        size: 25,
-                        color: context.colors.secondary,
-                      ),
+                    TextFormField(
+                        controller: _userController,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(20),
+                        ],
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.person,
+                            size: 25,
+                            color: context.colors.secondary,
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'O campo usuário não pode estar vazio';
+                          } else if (value != value.trim()) {
+                            return 'O usuário não pode ter espaços no final';
+                          }
+                          return null;
+                        }),
+                    SizedBox(
+                      height: context.percentHeight(.04),
                     ),
-                  ),
-                  SizedBox(
-                    height: context.percentHeight(.04),
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text(
-                        "Senha",
-                       style: context.textStyles.textLight.copyWith(
-                          color: context.colors.textAux,
-                          fontSize: 16,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: Text(
+                          "Senha",
+                          style: context.textStyles.textLight.copyWith(
+                            color: context.colors.whiteAux,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        size: 25,
-                        color: context.colors.secondary,
+                    TextFormField(
+                      controller: _passwordController,
+                      inputFormatters: [
+                        LengthLimitingTextInputFormatter(20),
+                      ],
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          size: 25,
+                          color: context.colors.secondary,
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'O campo senha não pode estar vazio';
+                        } else if (value.length < 2) {
+                          return 'O campo senha não pode ter menos que dois caracteres';
+                        } else if (!RegExp(r'^[a-zA-Z0-9 ]+$').hasMatch(value)) {
+                          return 'A senha deve conter apenas letras de A a Z e números de 0 a 9';
+                        } else if (value != value.trim()) {
+                          return 'A senha não pode ter espaços no final';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: context.percentHeight(.03),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: context.colors.greenAux,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        minimumSize: Size(
+                          context.percentWidth(.4),
+                          context.percentHeight(.06),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _onLoading();
+                        }
+                      },
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(color: context.colors.whiteAux),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: context.percentHeight(.03),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: context.colors.buttonHomePage,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      minimumSize: Size(
-                        context.percentWidth(.4),
-                        context.percentHeight(.06),
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      "Entrar",
-                      style: TextStyle(color: context.colors.textAux),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomCenter,
                   child: Padding(
                     padding: const EdgeInsets.only(bottom: 18),
-                    child: Text(
-                      "Política de Privacidade",
-                     style: context.textStyles.textLight.copyWith(
-                          color: context.colors.textAux,
+                    child: GestureDetector(
+                      onTap: () async {
+                      await urlLauncher.launch(Uri.parse('https://www.google.com'));                    
+                      },
+                      child: Text(
+                        "Política de Privacidade",
+                        style: context.textStyles.textLight.copyWith(
+                          color: context.colors.whiteAux,
                           fontSize: 16,
                         ),
+                      ),
                     ),
                   ),
                 ),
